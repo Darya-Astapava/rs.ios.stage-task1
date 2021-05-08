5b19e3cdf5c90cd140a4c0b3c6a15afb746775af
Daria Astapova <>
2021-05-08 20:25:54 +0300

    feat: implement tasks

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-2/NSString+ValidNumber.m
@@ -3,7 +3,17 @@
 @implementation NSString (ValidNumber)
 
 - (BOOL)isValidNumber {
-    return false;
+    
+    NSCharacterSet *numberSet = [NSCharacterSet decimalDigitCharacterSet];
+    NSString *trimmedString = [self stringByTrimmingCharactersInSet:numberSet];
+    if (self.length >0 && trimmedString.length == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 @end
--- Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
@@ -3,7 +3,62 @@
 @implementation NSArray (MinRotated)
 
 - (NSNumber *)minRotated {
-    return nil;
+    if (self.count == 0) {
+        return nil;
+    }
+    
+    if (self.count == 1)
+    {
+        return self[0];
+    }
+    
+    if (self.count == 2)
+    {
+        if ([self[0] integerValue] > [self[1] integerValue])
+        {
+            return self[1];
+        }
+        else
+        {
+            return self[0];
+        }
+    }
+    
+    NSUInteger left = 0;
+    NSUInteger right = self.count - 1;
+    
+    if ([self[0] integerValue] < [self[right] integerValue])
+    {
+        return self[0];
+    }
+    else
+    {
+        while (right >= left) {
+            NSUInteger mid = left + (right - left) / 2;
+            
+            if ([self[mid] integerValue] > [self[mid + 1] integerValue])
+            {
+                return self[mid + 1];
+            }
+            
+            if ([self[mid - 1] integerValue] > [self[mid] integerValue])
+            {
+                return self[mid];
+            }
+            
+            if ([self[mid] integerValue] > [self[0] integerValue])
+            {
+                left = mid + 1;
+            } else
+            {
+                right = mid - 1;
+            }
+        }
+        return nil;
+    }
 }
 
+
 @end
+
+
